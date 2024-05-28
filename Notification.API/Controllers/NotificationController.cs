5ab310b27f361a9f3dcd277d6477146c3b8ce184
Asadullokh27 <asadullohtojiyev696@gmail.com>
2024-05-28 16:29:13 +0500

    Add project files.

---
+++ Notification.API/Controllers/NotificationController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using Notification.API.Hubs;
+
+namespace Notification.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotificationController : ControllerBase
+    {
+
+        private readonly IHubContext<NotificationHub> _hubContext;
+
+        public NotificationController(IHubContext<NotificationHub> hubContext)
+        {
+            _hubContext = hubContext;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SendNotification([FromBody] string message)
+        {
+            await _hubContext.Clients.All.SendAsync("ReceiveNotification", message);
+            return Ok();
+        }
+
+    }
+}
